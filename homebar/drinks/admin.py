df4ea2f49197775362f3a784744de622fd7569c9
Chathan Driehuys <chathan@driehuys.com>
2021-09-02 21:14:13 +0000

    feat: Add drink recipes.

--- homebar/drinks/admin.py
+++ homebar/drinks/admin.py
@@ -1,14 +1,35 @@
+from typing import TYPE_CHECKING
+
 from django.contrib import admin
 from django.contrib.admin.decorators import register
+from django.db.models import Count
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+
 from drinks import models
 
 
 @register(models.Drink)
 class DrinkAdmin(admin.ModelAdmin):
     fields = ("name", "slug", "description")
-    list_display = ("name",)
+    list_display = ("name", "recipe_count")
     readonly_fields = ("slug",)
     search_fields = ("name",)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        queryset = super().get_queryset(request)
+
+        return queryset.annotate(recipe_count=Count("recipe"))
+
+    def recipe_count(self, obj) -> int:
+        return obj.recipe_count
+
+
+@register(models.DrinkRecipe)
+class DrinkRecipeAdmin(admin.ModelAdmin):
+    fields = ("drink", "variation", "notes", "ingredients")
+    list_display = ("drink", "variation")
+    search_fields = ("drink__name", "variation")
 
 
 @register(models.Ingredient)
--- homebar/drinks/models.py
+++ homebar/drinks/models.py
@@ -7,6 +7,9 @@ class Drink(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=50, primary_key=True)
 
+    def __str__(self) -> str:
+        return self.name
+
     def save(self, *args, **kwargs) -> None:
         # Only generate a new slug if one has not been saved. This avoids
         # breaking existing links.
@@ -14,6 +17,26 @@ class Drink(models.Model):
             self.slug = slugify(self.name)[:50]
 
         super().save(*args, **kwargs)
+
+
+class DrinkRecipe(models.Model):
+    drink = models.ForeignKey(
+        "drinks.Drink",
+        on_delete=models.CASCADE,
+        related_name="recipes",
+        related_query_name="recipe"
+    )
+    ingredients = models.ManyToManyField(
+        "drinks.Ingredient",
+        blank=True,
+        related_name="recipes",
+        related_query_name="recipe",
+    )
+    notes = models.TextField(blank=True)
+    variation = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.drink} - {self.variation}"
 
 
 class Ingredient(models.Model):
