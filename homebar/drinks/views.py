789bf7b56c19c48059a308e2977c9f3166bf062b
Chathan Driehuys <chathan@driehuys.com>
2021-09-04 14:07:02 +0000

    feat: In stock drinks.
    Only list drinks with a creatable recipe.

--- homebar/drinks/views.py
+++ homebar/drinks/views.py
@@ -1,8 +1,25 @@
+from django.db.models.expressions import F
+from django.db.models.query import QuerySet
+from django.db.models.query_utils import Q
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.db.models import Count
 
 from drinks import models
 
 
 class DrinksListView(ListView):
     model = models.Drink
+
+    def get_queryset(self) -> QuerySet[models.Drink]:
+        queryset = super().get_queryset()
+
+        # Exclude recipes that are missing an ingredient.
+        creatable_recipes = models.DrinkRecipe.objects.exclude(
+            ingredients__in_stock=False
+        )
+
+        # Only show drinks that have a creatable recipe.
+        queryset = queryset.filter(recipe__in=creatable_recipes).distinct()
+
+        return queryset
